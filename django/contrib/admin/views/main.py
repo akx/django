cb1c91d49d427d6b57bdc44ac20f543bef22688d
Aarni Koskela <akx@iki.fi>
2026-03-07 18:24:10 +0200

    Canonicalized unused variable name `none` to `_`.

--- django/contrib/admin/views/main.py
+++ django/contrib/admin/views/main.py
@@ -392,7 +392,7 @@ class ChangeList:
             order_params = params[ORDER_VAR].split(".")
             for p in order_params:
                 try:
-                    none, pfx, idx = p.rpartition("-")
+                    _, pfx, idx = p.rpartition("-")
                     field_name = self.list_display[int(idx)]
                     order_field = self.get_ordering_field(field_name)
                     if not order_field:
@@ -454,7 +454,7 @@ class ChangeList:
                         break
         else:
             for p in self.params[ORDER_VAR].split("."):
-                none, pfx, idx = p.rpartition("-")
+                _, pfx, idx = p.rpartition("-")
                 try:
                     idx = int(idx)
                 except ValueError:
